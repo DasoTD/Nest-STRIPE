ebeb3defc3b90d4af7124f522c5fb72a8466a506
David <dasodavid4@gmail.com>
2023-02-27 09:08:22 +0000

    environment variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/mongo')],
+  imports: [ ConfigModule.forRoot({
+    envFilePath: '.env',
+    isGlobal: true
+  }),
+    MongooseModule.forRoot(process.env.MONGO_URL),
+],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+//I88HBVhDHoeNewvT
+
+// mongodb+srv://dasodavid:<password>@youkay.mxctl7i.mongodb.net/?retryWrites=true&w=majority
