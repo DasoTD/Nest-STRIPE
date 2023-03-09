064b6ef81a34d5598963c498ea66843dce4753a8
David <dasodavid4@gmail.com>
2023-03-09 21:10:55 +0000

    user repo constructor

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(private userRepo: UserRepository){}
+  async create(createAuthDto: CreateAuthDto) {
+    const {username, password} = createAuthDto
+    const user =await this.userRepo.createUser(createAuthDto)
+    return user; //'This action adds a new auth';
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  async findAll() {
+    let users = await this.userRepo.findAndCount()
+    return users; //`This action returns all auth`;
   }
 
   findOne(id: number) {
